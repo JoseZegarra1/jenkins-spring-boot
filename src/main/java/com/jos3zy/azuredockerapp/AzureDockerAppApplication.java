c48d43afe01cb0493318177d2560d27d3d342c96
Jose Zegarra <jose.zegarra@vallegrande.edu.pe>
2024-01-11 06:56:58 -0500

    logger.info

--- src/main/java/com/jos3zy/azuredockerapp/AzureDockerAppApplication.java
+++ src/main/java/com/jos3zy/azuredockerapp/AzureDockerAppApplication.java
@@ -1,13 +1,23 @@
 package com.jos3zy.azuredockerapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AzureDockerAppApplication {
+public class AzureDockerAppApplication implements CommandLineRunner {
+
+	public static final Logger logger = LoggerFactory.getLogger(AzureDockerAppApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Starting AzureDockerAppApplication");
 		SpringApplication.run(AzureDockerAppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Running AzureDockerAppApplication run method");
+	}
 }
